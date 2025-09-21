1e1f0a9ec867a5aa517adad2b4e54ae0959ba04a
Ege Süalp <egesualp@gmail.com>
2025-09-21 17:10:16 +0200

    first commit

---
+++ src/train.py
@@ -0,0 +1,10 @@
+from skills4cpp.model import Model
+from skills4cpp.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
